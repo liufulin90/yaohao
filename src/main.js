f789eb419d7ae85dc5055db6469a5c487e0bfbb4
liufulin90 <liufulin90@163.com>
2018-05-16 16:23:41 +0800

    允许qq访问

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ new Vue({
 if (process.env.NODE_ENV === 'production') {
   // 对浏览器的UserAgent进行正则匹配，不含有微信独有标识的则为其他浏览器
   var useragent = navigator.userAgent
-  if (useragent.match(/MicroMessenger/i) != 'MicroMessenger') {
+  if (useragent.match(/MicroMessenger/i) != 'MicroMessenger' && useragent.match(/QQ/i) != 'QQ') {
     // 这里警告框会阻塞当前页面继续加载
     alert('已禁止本次访问：您必须使用微信内置浏览器访问本页面！')
     // 以下代码是用javascript强行关闭当前页面
